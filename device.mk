3c02d1716a3b139317519f22b4eabf5ebe1510f5
Woomymy <woomy@woomy.be>
2023-04-05 17:21:39 +0200

    cancunf: Add and Build dummy libpowerhalwrap_vendor library
    * Required by some vendor blobs that depend on it
    
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I6d0b16800a372cb0094fc4cf910b01d3fe05a37e

--- device.mk
+++ device.mk
@@ -349,6 +349,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     libmtkperf_client_vendor \
     libmtkperf_client \
+    libpowerhalwrap_vendor \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub \
     vendor.mediatek.hardware.mtkpower@1.2.vendor
 
