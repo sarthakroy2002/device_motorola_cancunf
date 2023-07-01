5f4f400daa66f6b6c325851a31dadf310bdbebd1
Christian Oder <myself5@carbonrom.org>
2023-07-01 17:20:17 +0700

    cancunf: Build disabled VBMeta image
    Change-Id: I48e3ff106a2e4e044ed39b86a94f3bc80b93f530
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,5 +29,9 @@ TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := mt6855
 BOARD_HAS_MTK_HARDWARE := true
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
 # Inherit the proprietary files
 include vendor/motorola/cancunf/BoardConfigVendor.mk
