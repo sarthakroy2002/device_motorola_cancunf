8e92986b422b37c81f687df9d74db3386c18199d
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-09 00:15:27 +0530

    cancunf: Set `TARGET_PRODUCT`
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I985e87e57ed5e25442f39d9e6000f28da2aaefdf

--- arrow_cancunf.mk
+++ arrow_cancunf.mk
@@ -23,6 +23,7 @@ PRODUCT_MODEL := moto g54 5G
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=cancunf_g_sysq \
     PRIVATE_BUILD_DESC="cancunf_g_sysq-user 13 T3TD33.16-51-1-1 879234-74307 release-keys"
 
 BUILD_FINGERPRINT := motorola/cancunf_g_sysq/cancunf:13/T3TD33.16-51-1-1/879234-74307:user/release-keys
