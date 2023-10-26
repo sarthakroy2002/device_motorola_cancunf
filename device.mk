6bdf0cba84409066eb0be110f3ab1406112ddad2
Mashopy <eliasgheeraert@gmail.com>
2023-10-26 21:18:53 +0530

    cancunf: Copy fstab to vendor_ramdisk
    Change-Id: I1e3d864eb534ce341f6e8c7feaa9494d16f5ad8f

--- device.mk
+++ device.mk
@@ -86,6 +86,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.mt6855 \
     fstab.mt6855.ramdisk \
+    fstab.mt6855.vendor_ramdisk \
     init_connectivity.rc \
     init.cgroup.rc \
     init.connectivity.common.rc \
