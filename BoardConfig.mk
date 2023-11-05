99552bf4df8386d6c01f74fb44ca9df5ac2eeba4
Erfan Abdi <erfangplus@gmail.com>
2023-11-05 20:49:57 +0530

    cancunf: Import moto GIDs
    Change-Id: I4b05aed79e878a53315cdea0974ee0cd1c14ff22

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,9 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY := 400
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/mot_aids.fs
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
