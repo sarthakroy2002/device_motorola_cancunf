f08be1be7168029b314138401cf3132ed2e53ad3
Sarthak Roy <sarthakroy2002@gmail.com>
2023-11-13 11:44:10 +0530

    cancunf: Move Moto scripts to proprietary files
    * We wont modify those
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Id1cc74711657bbc15db90cf8fd68a32c3588ff67

--- device.mk
+++ device.mk
@@ -277,33 +277,12 @@ PRODUCT_PACKAGES += \
 
 # Rootdir
 PRODUCT_PACKAGES += \
-    hardware_revisions.sh \
-    init.insmod.sh \
-    init.mmi.backup.trustlet.sh \
-    init.mmi.block_perm.sh \
-    init.mmi.boot.sh \
-    init.mmi.modem-rfs.sh \
-    init.mmi.modules.sh \
-    init.mmi.shutdown.sh \
-    init.mmi.touch.sh \
-    init.mmi.usb.sh \
-    init.oem.fingerprint2.sh \
-    init.oem.hw.sh \
-    vendor.mmi.cxp.sh
-
-PRODUCT_PACKAGES += \
     fstab.mt6855 \
     fstab.mt6855.ramdisk \
     fstab.mt6855.vendor_ramdisk \
     init_connectivity.rc \
     init.cgroup.rc \
     init.connectivity.common.rc \
-    init.mmi.backup.trustlet.rc \
-    init.mmi.chipset.rc \
-    init.mmi.overlay.rc \
-    init.mmi.rc \
-    init.mmi.tcmd.rc \
-    init.mmi.usb.configfs.rc \
     init.modem.rc \
     init.mt6855.rc \
     init.mt6855.usb.rc \
