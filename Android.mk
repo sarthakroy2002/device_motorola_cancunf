9871ea5e13cee3bf964f648cb6d640007a8cd1cd
Sarthak Roy <sarthakroy2002@gmail.com>
2023-12-07 11:01:09 +0530

    cancunf: Setup Vendor Symlinks
    Co-authored-by: electimon <electimon@gmail.com>
    Change-Id: If8f1343a1bfb670c4846c23f06f2f7daa0914af3

--- Android.mk
+++ Android.mk
@@ -8,4 +8,27 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),cancunf)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+CANCUNF_SYMLINK := $(addprefix $(TARGET_OUT_VENDOR)/, $(strip $(shell cat $(LOCAL_PATH)/symlinks.txt)))
+$(CANCUNF_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf mt6855/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(CANCUNF_SYMLINK)
+
+ADDITIONAL_SYMLINKS := \
+    $(TARGET_OUT_VENDOR)/lib/hw \
+    $(TARGET_OUT_VENDOR)/lib64/hw
+
+$(ADDITIONAL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
+	@ln -sf /vendor/bin/hw/mt6855/android.hardware.graphics.allocator@4.0-service-mediatek.mt6855 $(TARGET_OUT_VENDOR)/bin/hw/android.hardware.graphics.allocator@4.0-service-mediatek
+	@ln -sf /vendor/bin/mt6855/v3avpud.mt6855 $(TARGET_OUT_VENDOR)/bin/v3avpud
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(ADDITIONAL_SYMLINKS)
+
 endif
