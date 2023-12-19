161c2a203a1c7de3b2a7d0a003df3450ce60c445
electimon <electimon@gmail.com>
2023-12-19 19:09:48 +0530

    cancunf: Copy recovery init to recovery root
    Change-Id: Ie11897dd2474395bad7318d4e4511180a227c107

--- device.mk
+++ device.mk
@@ -315,6 +315,7 @@ PRODUCT_PACKAGES += \
     ueventd.mt6855.rc
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.recovery.mt6855.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mt6855.rc \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.mt6855.rc:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/init.recovery.mt6855.rc
 
 # Sensors
