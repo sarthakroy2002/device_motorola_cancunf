7818558bd01a84cd84493fbd1e2c817ef615498f
Sarthak Roy <sarthakroy2002@gmail.com>
2023-12-29 00:08:41 +0530

    cancunf: Add more symlinks
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ifa31940ea42a15dad0788c088d3eeff879d45fa7

--- Android.mk
+++ Android.mk
@@ -19,17 +19,60 @@ $(CANCUNF_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(CANCUNF_SYMLINK)
 
-ADDITIONAL_SYMLINKS := \
-    $(TARGET_OUT_VENDOR)/lib/hw \
-    $(TARGET_OUT_VENDOR)/lib64/hw
+AUDIO_PRIMARY_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/audio.primary.mt6855.so
+$(AUDIO_PRIMARY_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf audio.primary.mediatek.so $@
 
-$(ADDITIONAL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
-	@ln -sf /vendor/bin/hw/mt6855/android.hardware.graphics.allocator@4.0-service-mediatek.mt6855 $(TARGET_OUT_VENDOR)/bin/hw/android.hardware.graphics.allocator@4.0-service-mediatek
-	@ln -sf /vendor/bin/mt6855/v3avpud.mt6855 $(TARGET_OUT_VENDOR)/bin/v3avpud
-	$(hide) touch $@
+ALL_DEFAULT_INSTALLED_MODULES += $(AUDIO_PRIMARY_SYMLINK)
 
-ALL_DEFAULT_INSTALLED_MODULES += $(ADDITIONAL_SYMLINKS)
+AUDIO_SUBMIX_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/audio.r_submix.mt6855.so
+$(AUDIO_SUBMIX_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf audio.r_submix.mediatek.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(AUDIO_SUBMIX_SYMLINK)
+
+GATEKEEPER_DEFAULT_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+$(GATEKEEPER_DEFAULT_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libSoftGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_DEFAULT_SYMLINK)
+
+GATEKEEPER_TRUSTONIC_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.trustonic.so
+$(GATEKEEPER_TRUSTONIC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_TRUSTONIC_SYMLINK)
+
+GRAPHICS_SYMLINK += $(TARGET_OUT_VENDOR)/bin/hw/android.hardware.graphics.allocator@4.0-service-mediatek
+$(GRAPHICS_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf mt6855/android.hardware.graphics.allocator@4.0-service-mediatek.mt6855 $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GRAPHICS_SYMLINK)
+
+KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/kmsetkey.default.so
+$(KMSETKEY_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf kmsetkey.trustonic.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KMSETKEY_SYMLINK)
+
+SENSORS_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/sensors.mt6855.so
+$(SENSORS_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf sensors.mediatek.V2.0.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SENSORS_SYMLINK)
+
+VPUD_SYMLINK += $(TARGET_OUT_VENDOR)/bin/v3avpud
+$(VPUD_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf mt6855/v3avpud.mt6855 $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VPUD_SYMLINK)
 
 endif
