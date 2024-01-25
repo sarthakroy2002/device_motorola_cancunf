b63352ba18c08d4dfc26fa791d9a094b1647b9f2
Sarthak Roy <sarthakroy2002@gmail.com>
2024-01-25 12:50:19 +0530

    !fixup: cancunf: Add script to update sha1sums of kanged blobs
    Change-Id: I295a33b834ca0d0ef8f26fe732783b685238a104

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -20,8 +20,8 @@ import os
 import sys
 from hashlib import sha1
 
-device='RMX2020'
-vendor='realme'
+device='cancunf'
+vendor='motorola'
 
 with open('proprietary-files.txt', 'r') as f:
     lines = f.read().splitlines()
