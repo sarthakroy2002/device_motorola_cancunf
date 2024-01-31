3ced3dbec29e4cef0da36f636314e238088786d1
Erfan Abdi <erfangplus@gmail.com>
2024-01-31 17:22:40 +0330

    cancunf: Switch to shared libs to MediaTek framework jars
    - This is required to get IMS working in A16 QPR1.
    
    Change-Id: I9d28da23ca7fdfb7a237b69d4624a86d2c096b9f
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -169,7 +169,11 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml \
+    $(DEVICE_PATH)/configs/permissions/mediatek-common.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-common.xml \
+    $(DEVICE_PATH)/configs/permissions/mediatek-framework.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-framework.xml \
+    $(DEVICE_PATH)/configs/permissions/mediatek-ims-base.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-ims-base.xml \
+    $(DEVICE_PATH)/configs/permissions/mediatek-ims-oem-plugin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-ims-oem-plugin.xml
 
 # Init
 PRODUCT_PACKAGES += \
