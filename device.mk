2906a3879e6154287188b1193d8bba973ad8e900
bengris32 <bengris32@protonmail.ch>
2024-02-21 07:51:11 +0530

    cancunf: Build wpa_supplicant from source
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I8db539db3f1aab8d56b6a1acd0062d8bd8b5bbbf

--- device.mk
+++ device.mk
@@ -398,6 +398,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     hostapd \
+    wpa_supplicant \
     android.hardware.wifi.supplicant@1.4.vendor \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
