2bc306ed6617470decd189c3b05218a17c2c55d2
Sarthak Roy <sarthakroy2002@gmail.com>
2024-02-27 19:17:34 +0530

    cancunf: Build `DolbyManager`
    * Drop stock dolby dax and motodolby deps from proprietary listings
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: If06444bd0321005fa756aee057cb32e4b3dc8ddf

--- device.mk
+++ device.mk
@@ -82,6 +82,9 @@ PRODUCT_PACKAGES += \
     libaudiofoundation.vendor \
     libtinycompress
 
+PRODUCT_PACKAGES += \
+    DolbyManager
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
