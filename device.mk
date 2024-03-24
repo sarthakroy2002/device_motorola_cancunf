f4647a7e2f2bebf899f79a87e06a01579ec8b8e9
bengris32 <bengris32@protonmail.ch>
2024-03-24 11:47:49 +0530

    cancunf: Build `libwifi-hal-wrapper`
    * In U QPR2, new vendor HAL functions were added to wifi_hal_fn,
      this struct defines all possible functions that can be implemented
      by a Wi-Fi vendor HAL.
    
    * The issue here is that, the new members were not added to the
      end of the struct, which causes misalignment issues when trying
      to load a vendor HAL that was built against an older version.
    
    * Introduce a wrapper which uses the legacy function pointer table
      that these older HALs were compiled against. This is a cleaner solution
      compared to forking the entire Wi-Fi AIDL service.
    
    Change-Id: I4db7448c64f05de23dbd05150aff2895b26015a3

--- device.mk
+++ device.mk
@@ -452,6 +452,9 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
 
+PRODUCT_PACKAGES += \
+    libwifi-hal-wrapper
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
