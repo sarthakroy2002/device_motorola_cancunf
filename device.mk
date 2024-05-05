79472a790fd1aae75506d1e35d865af73cd71d4d
Sarthak Roy <sarthakroy2002@gmail.com>
2024-05-05 22:14:07 +0530

    Revert "cancunf: Ship AOSP cgroups and task_profiles configs"
    This reverts commit ffd666e5ee75dda70f74b9d3c90cd61f9914e6cb.

--- device.mk
+++ device.mk
@@ -124,8 +124,7 @@ PRODUCT_PACKAGES += \
 
 # Cgroup
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Display
 PRODUCT_PACKAGES += \
