c35ad09091f4c9a2a8f6b95546eb8d009760e008
Sarthak Roy <sarthakroy2002@gmail.com>
2024-07-31 02:08:20 +0530

    cancunf: Update to `U1TDS34.94-12-7-6`
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ica6616c4b5ee243e3fda153126fa644096aa36b1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,7 +150,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-05-01
+VENDOR_SECURITY_PATCH := 2024-07-01
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
