285de190b11d2f243fa8c51b4674949a387b1686
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-03 08:36:46 +0530

    cancunf: dolby: Introduce graphic equalizer
    Squashed:
    
    dolby: Refresh preset name on main screen
    
    Change-Id: I96783e2a03c384f031787f4cc9140f7d64dadb2f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I38ee6ce594e5671af42afc3d4bf0f004329482b9

--- app/DolbyManager/src/co/aospa/settings/dolby/DolbySettingsFragment.kt
+++ app/DolbyManager/src/co/aospa/settings/dolby/DolbySettingsFragment.kt
@@ -34,7 +34,7 @@ class DolbySettingsFragment : PreferenceFragment(),
         findPreference<ListPreference>(DolbyConstants.PREF_PROFILE)!!
     }
     private val presetPref by lazy {
-        findPreference<ListPreference>(DolbyConstants.PREF_PRESET)!!
+        findPreference<Preference>(DolbyConstants.PREF_PRESET)!!
     }
     private val stereoPref by lazy {
         findPreference<ListPreference>(DolbyConstants.PREF_STEREO)!!
@@ -103,7 +103,6 @@ class DolbySettingsFragment : PreferenceFragment(),
             }
         }
 
-        presetPref.onPreferenceChangeListener = this
         hpVirtPref.onPreferenceChangeListener = this
         spkVirtPref.onPreferenceChangeListener = this
         stereoPref.onPreferenceChangeListener = this
@@ -132,6 +131,11 @@ class DolbySettingsFragment : PreferenceFragment(),
         super.onDestroyView()
     }
 
+    override fun onResume() {
+        super.onResume()
+        updateProfileSpecificPrefs()
+    }
+
     override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
         dlog(TAG, "onPreferenceChange: key=${preference.key} value=$newValue")
         when (preference.key) {
@@ -140,10 +144,6 @@ class DolbySettingsFragment : PreferenceFragment(),
                 dolbyController.profile = profile
                 (preferenceManager.preferenceDataStore as DolbyPreferenceStore).profile = profile
                 updateProfileSpecificPrefs()
-            }
-
-            DolbyConstants.PREF_PRESET -> {
-                dolbyController.setPreset(newValue.toString())
             }
 
             DolbyConstants.PREF_SPK_VIRTUALIZER -> {
@@ -205,15 +205,7 @@ class DolbySettingsFragment : PreferenceFragment(),
 
         if (!enable) return
 
-        val preset = dolbyController.getPreset(currentProfile)
-        presetPref.apply {
-            if (entryValues.contains(preset)) {
-                summary = "%s"
-                value = preset
-            } else {
-                summary = unknownRes
-            }
-        }
+        presetPref.summary = dolbyController.getPresetName()
 
         val deValue = dolbyController.getDialogueEnhancerAmount(currentProfile).toString()
         dialoguePref.apply {
