219db7d8479c4105df39711006f9e8db7bb3f256
Ethan Chen <intervigil@gmail.com>
2024-08-07 22:43:16 +0530

    cancunf: Declare EUICC permission
    This is a squash of the following:
    
    Author: Ethan Chen <intervigil@gmail.com>
    Date:   Sat Dec 1 13:53:00 2018 -0800
    
        Declare EUICC permission
    
        Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Fri Oct 18 00:06:22 2019 -0500
    
        Move eUICC permissions xml to /product
    
        All eUICC items were moved to /product in Q.
    
        Change-Id: I614f86b16f61959c47465863ab1d5d55030209ab
    
    Change-Id: I323dce8b848b44e45ed21002950059e58719113c

--- device.mk
+++ device.mk
@@ -373,6 +373,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
