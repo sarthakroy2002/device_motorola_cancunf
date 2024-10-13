6d9af26dc0684342f946a3aa1d4cc280167786e9
Sarthak Roy <sarthakroy2002@gmail.com>
2024-10-13 11:18:41 +0530

    Revert "cancunf: Build AOSP AIDL SecureElement NXP service"
    This reverts commit cf7b08f4bf25529aad057529b009c2e39a59cfb5.

--- device.mk
+++ device.mk
@@ -213,8 +213,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.nxp \
-    android.hardware.secure_element-service.nxp
+    android.hardware.nfc-service.nxp
 
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
