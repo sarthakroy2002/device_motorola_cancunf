f508dc8e2dce2b1b07fbd651dd88712db1ae02a0
Sarthak Roy <sarthakroy2002@gmail.com>
2025-07-14 23:01:37 +0530

    cancunf: Cleanup Lights AIDL
    - Cleanup notification led support
    
    Change-Id: Ib5d6d1e64fcd462b543a863d4f52984880a12110
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- lights/Lights.h
+++ lights/Lights.h
@@ -18,27 +18,24 @@
 #pragma once
 
 #include <aidl/android/hardware/light/BnLights.h>
-#include <array>
+#include <android-base/file.h>
+#include <android-base/logging.h>
+
+using ::aidl::android::hardware::light::HwLightState;
+using ::aidl::android::hardware::light::HwLight;
+using ::aidl::android::hardware::light::LightType;
+using ::aidl::android::hardware::light::BnLights;
 
 namespace aidl {
 namespace android {
 namespace hardware {
 namespace light {
 
-// Keep sorted in the order of priority.
-constexpr std::array kAvailableLights = {
-        // id, ordinal, type
-        HwLight{static_cast<int32_t>(LightType::NOTIFICATIONS), 0, LightType::NOTIFICATIONS},
-        HwLight{static_cast<int32_t>(LightType::BATTERY), 0, LightType::BATTERY},
-};
-
 class Lights : public BnLights {
   public:
     ndk::ScopedAStatus setLightState(int id, const HwLightState& state) override;
     ndk::ScopedAStatus getLights(std::vector<HwLight>* types) override;
 
-  private:
-    std::array<HwLightState, kAvailableLights.size()> notif_states_;
 };
 
 }  // namespace light
