aae310620dec04301d6d431ce3de261f4a91ea5f
Mashopy <eliasgheeraert@gmail.com>
2025-08-18 22:33:59 +0900

    cancunf: Switch to AIDL bluetooth service
    Change-Id: I40550f0f2033e35ea609401f38f7646cb79abe14
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -85,7 +85,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio-impl
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth-service.mediatek
 
 # Boot
 PRODUCT_PACKAGES += \
