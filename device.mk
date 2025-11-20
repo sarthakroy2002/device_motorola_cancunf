840dc967c74cc3146f7f36e732f142a08d46ddbc
claxten10 <claxten10@gmail.com>
2025-11-20 22:54:35 +0000

    cancunf: Switch to dummy libaedv
    Change-Id: I0f87e8536103cd59b41a2d8ee322c861a352e31d
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -412,6 +412,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/mediatek \
+    hardware/mediatek/libaedv \
     hardware/mediatek/libmtkperf_client \
     hardware/mediatek/wlan/wifi_hal \
     $(DEVICE_PATH)
--- extract-files.py
+++ extract-files.py
@@ -20,6 +20,7 @@ from extract_utils.main import (
 namespace_imports = [
     'device/motorola/cancunf',
     'hardware/mediatek',
+    'hardware/mediatek/libaedv',
     'hardware/mediatek/libmtkperf_client',
 ]
 
