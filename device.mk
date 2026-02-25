b0940022fa318f835cac47c148cd63cf2d0be87d
Sarthak Roy <sarthakroy2002@gmail.com>
2026-02-25 15:58:49 +0000

    cancunf: Copy Keystore manifest for Keymint V1
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I543f806860d7d53ef3a9ae8a9bd1cea3b051703f

--- device.mk
+++ device.mk
@@ -203,7 +203,7 @@ PRODUCT_PACKAGES += \
 
 # Keystore
 PRODUCT_PACKAGES += \
-    android.hardware.hardware_keystore.xml \
+    android.hardware.hardware_keystore_V1.xml \
     android.system.wifi.keystore@1.0.vendor \
     libkeystore-wifi-hidl \
     libkeystore-engine-wifi-hidl
