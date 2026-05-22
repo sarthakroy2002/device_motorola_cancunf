944cac8e38e739af674cad2ca8b761d33065c34f
Cyber Knight <cyberknight755@gmail.com>
2026-05-22 19:48:36 +0800

    cancunf: Import fingerprint init script from `V1TDS35H.83-20-5-10`
    To be improved upon in future commits.
    
    Change-Id: I90bdc8ec2305ac99d3828678e68e5c454d3e23f8
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -183,6 +183,7 @@ PRODUCT_PACKAGES += \
     init.mt6855.power.rc \
     init.mt6855.usb.rc \
     init.mtkgki.rc \
+    init.oem.fingerprint2.sh \
     init.oem.hw.sh \
     init.project.rc \
     init.recovery.mt6855.rc \
